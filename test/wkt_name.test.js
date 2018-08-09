5b6f3a64325371c361a0b667c010a1fc474f1a31
Michael Bykov <m.bykov@gmail.com>
2018-08-09 17:53:10 +0300

    i

--- test/wkt_name.test.js
+++ test/wkt_name.test.js
@@ -27,8 +27,9 @@ process.prependListener("exit", (code) => {
   }
 })
 
-let upath = path.resolve(__dirname, '../../')
-enableDBs(upath)
+let upath = path.resolve(process.env.HOME, '.config/MorpheusGreek (development)')
+let apath = path.resolve(__dirname, '../../egreek')
+enableDBs(upath, apath)
 
 const testpath = path.resolve(__dirname, 'wkt_name.txt')
 const text = fse.readFileSync(testpath,'utf8')
@@ -37,86 +38,62 @@ let param = process.argv.slice(2)[1]
 
 let skip = true
 
-let cases = ['nom', 'gen', 'dat', 'acc', 'voc']
-let nums // = ['sg', 'du', 'pl']
+// let cases = ['nom', 'gen', 'dat', 'acc', 'voc']
+let numbers = ['sg', 'du', 'pl']
 
 let rtests = []
 let tdoc
 let rows = text.split('\n')
+
+let dict
+let formstr
+let store = []
+
 rows.forEach((row, idx) => {
-  if (!row) return
-  if (row[0] == '#') return
-  // if (idx > 12) return
-  row = row.trim()
-  if (row == 'MA')
-  { skip = false
-    return }
+  // if (idx > 31) return
+  if (/MA/.test(row)) skip = false
   if (skip) return
-  // log('R', row)
-  let arr = row.split('dict: ')
-  if (arr.length > 1) {
-    // dict: ἀβακίσκος • (abakískos) m (genitive ἀβακίσκου); second declension
-    let gend
-    if (row.split(' m ').length > 1) gend = 'masc'
-    else if (row.split(' m, f ').length > 1) gend = 'masc fem'
-    else if (row.split(' f ').length > 1) gend = 'fem'
-    else if (row.split(' n ').length > 1) gend = 'neut'
-    let pl
-    if (row.split(' pl ').length > 1) pl = true
-    nums = ['sg', 'du', 'pl']
-    let txt = arr[1].trim()
-    let dict = txt.split('•')[0].trim()
-    tdoc = {dict: dict, gend: gend, numcases: {}}
-    if (pl) tdoc.pl = true
-    rtests.push(tdoc)
-  } else {
-    if (/Singular, Plural/.test(row)) {
-      nums = ['sg', 'pl']
-      return
-    } else if (/Dual, Plural/.test(row)) {
-      nums = ['du', 'pl']
-      return
-    } else if (/Singular/.test(row)) {
-      nums = ['sg']
-      return
-    } else if (/Plural/.test(row)) {
-      nums = ['pl']
-      return
-    }
-    let arr = row.split(': ')
-    let kase = arr[0]
-    if (!cases.includes(kase)) {
-      log('BAD CASE!', row, 'case:', kase, 'nums', nums)
-      throw new Error('case')
-    }
-    let args2 = arr[1].split(', ')
-    if (args2.length != nums.length) {
-      log('NO NUMBER!', row, 'nums:', nums,  'args2:', args2)
-      throw new Error('number')
-    }
-    args2.forEach((arg2, idx) => {
-      if (arg2 == '-') return
-      let args = arg2.split('-')
-      args.forEach(arg => {
-        let num = nums[idx]
-        let numcase = [num, kase].join('.')
-        tdoc.numcases[numcase] = arg.trim()
-      })
-    })
+  if (!row || row.slice(0,2) == '# ') return
+  if (row[0] == ' ') return
+  let descr = row.split(':')[0].trim()
+
+  if (descr == 'dict' || descr == 'END') {
+    let res =  {dict: dict, formstr: formstr, data: store} // ,
+    parseGend(res)
+    if (dict) rtests.push(res)
+    store = []
   }
 
+  if (descr == 'dict') {
+    let txt = row.split(':')[1].trim()
+    dict = txt.split('•')[0].trim()
+    formstr = txt.split('•')[1].trim()
+  } else if (['nom', 'gen', 'dat', 'acc', 'voc'].includes(descr)) {
+    let str = row.split(':')[1]
+    if (!str) return
+    let res = {descr: descr, forms: str.trim().split(', ')}
+    store.push(res)
+  }
 })
 
 let tests = []
 rtests.forEach(doc => {
-  for (let numcase in doc.numcases) {
-    // let morph = [doc.gend, numcase].join('.')
-    let test = [doc.dict, doc.numcases[numcase], doc.gend, numcase]
-    tests.push(test)
-  }
+  // let test = {dict: doc.dict}
+  doc.data.forEach(line => {
+    let kase = line.descr
+    line.forms.forEach((form2, idx) => {
+      if (!form2) return
+      form2.split('-').forEach(form => {
+        let number = numbers[idx]
+        let numcase = [number, kase].join('.')
+        let test = [doc.dict, form, doc.gend, numcase]
+        tests.push(test)
+      })
+    })
+  })
 })
 
-tests = _.compact(tests)
+// tests = _.compact(tests)
 // tests = tests.slice(0, 25)
 // console.log('T', tests)
 // tests = []
@@ -127,24 +104,19 @@ forEach(tests)
     antrax(arg)
       .then(chains => {
         if (!chains.length) log('NO RESULT'), assert.equal(false, true)
-        chains.forEach(chain => {
+        let corrchs = _.filter(chains, ch => { return ch[ch.length-2].dicts.map(dict => { return dict.rdict}).includes(rdict) })
+        if (!corrchs.length) log('no correct chains'), assert.equal(false, true)
+
+        corrchs.forEach(chain => {
           // log('CH.length', chain)
           if (chain.length > 2) log('CH.length'), assert.equal(false, true)
           let penult = chain[chain.length-2]
-          penult.dicts.forEach(dict => {
-            if (!dict.name) return // глаголы
-            if (!dict.gend) return // прилагательные
-
-            if (dict.gend != gend) return // лишние сущ. не того рода αἴθων αἶθος, ἀδελφή - ἀδελφός
-            // лишние решения, Γαλάτας - Γαλάτης
-            if (orthos.toComb(dict.rdict) != orthos.toComb(rdict)) return
-
+          let names = _.filter(penult.dicts, dict => { return dict.name })
+          if (!names.length) log('no name'), assert.equal(false, true)
+          let gends = _.filter(names, dict => { return dict.gend == gend })
+          if (!gends.length) log('no gend'), assert.equal(false, true)
+          gends.forEach(dict => {
             let fls = _.last(chain).flexes
-
-            // let exps = fls.map(flex => { return [flex.gend, flex.numcase].join('.') })
-            // пока тест на уникальность не проходит -is-idos, -is-eos имеют -is в sg.nom: ἄγρωστις
-            // assert.equal(exps.length, _.uniq(exps).length) // результаты fls не повторяются
-
             let numcases = fls.map(flex => { return flex.numcase })
             assert.equal(numcases.includes(numcase), true)
           })
@@ -152,3 +124,17 @@ forEach(tests)
       })
       .then(done)
   })
+
+function parseGend(res) {
+  let head = res.formstr
+  if (!head) return
+  let gend
+  if (head.split(' m ').length > 1) gend = 'masc'
+  else if (head.split(' f ').length > 1) gend = 'fem'
+  else if (head.split(' n ').length > 1) gend = 'neut'
+  else if (head.split(' m f ').length > 1) gend = 'masc fem'
+  if (head.split(' pl ').length > 1) res.pl = true
+  // if (!gend) log('H', par.rdict, head)
+  // if (!gend) return
+  res.gend = gend
+}
