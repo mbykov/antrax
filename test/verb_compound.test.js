08b11ca9fbd19cff750adf974738048cb9b1edf5
Michael Bykov <m.bykov@gmail.com>
2019-04-26 08:07:25 +0300

    i

--- test/verb_compound.test.js
+++ test/verb_compound.test.js
@@ -15,13 +15,11 @@ const fse = require('fs-extra')
 const path = require('path')
 
 let unhandledRejectionExitCode = 0
-
 process.on("unhandledRejection", (reason) => {
   // console.log("unhandled rejection:", reason)
   unhandledRejectionExitCode = 1
   throw reason
 })
-
 process.prependListener("exit", (code) => {
   if (code === 0) {
     process.exit(unhandledRejectionExitCode)
