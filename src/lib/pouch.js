4e8cf408cc6b91511b313235f26a16cb4614c4da
Michael Bykov <m.bykov@gmail.com>
2019-09-25 22:00:53 +0300

    v.0.9.117

--- src/lib/pouch.js
+++ src/lib/pouch.js
@@ -50,7 +50,7 @@ function checkCfg(apath, upath, dnames) {
       infos = _.filter(infos, dict=> { return dict.dname != 'flex' })
       let cfg = infos.map((dict, idx)=> { return {dname: dict.dname, idx: dict.idx, active: true, sync: true, size: dict.doc_count, langs: '', info: '' } } )
       log('--init-cfg--', cfg.length)
-      setDBs (upath, dnames)
+      setDBs(upath, dnames)
       return cfg
     })
 }
